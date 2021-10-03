2325a38fc05388a4244efe80277d05d745d9fe82
Farouk Goaudria <gouadriaafar@gmail.com>
2021-10-03 22:11:38 +0100

    First Commit

---
+++ src/Components/Note.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Note = () => {
+    return (
+        <div className="note">
+            <span>First Note</span>
+            
+        </div>
+    )
+}
+
+export default Note
---
+++ src/Components/NotesList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Note from './Note';
+const NotesList = () => {
+    return (
+        <div className="note_list">
+            <Note />
+        </div>
+    )
+}
+
+export default NotesList;
